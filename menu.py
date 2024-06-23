6bfdf0e6b67123904ab8f0cf010095bcf7394644
talosPy <tala0147@gmail.com>
2024-06-23 18:32:54 +0300

    icecream added

--- menu.py
+++ menu.py
@@ -1,19 +1,21 @@
 from dest.action import add_dest, edit_dest, delete_dest
 from dest.search import search, high_score
 from storage.load_save import save, load
+from icecream import ic # to import ic from icecream package (inside venv!!!)
+                        # !!!!! *ic* replaces the *print* using icecream !!!!!
 
 
 def menu():
     my_destinations = []
     load(my_destinations)
     while True: 
-        print('1 - Add Destinations')
-        print('2 - Edit Destinations')
-        print('3 - Delete Destinations')
-        print('4 - List Destinations')
-        print('5 - Search Destinations')
-        print('6 - Search Great Destinations')
-        print('7 - Exit Program')
+        ic('1 - Add Destinations')
+        ic('2 - Edit Destinations')
+        ic('3 - Delete Destinations')
+        ic('4 - List Destinations')
+        ic('5 - Search Destinations')
+        ic('6 - Search Great Destinations')
+        ic('7 - Exit Program')
         selection = input('Select command')
         if selection == '1':
             add_dest(my_destinations)
@@ -22,13 +24,13 @@ def menu():
         elif selection == '3':
             delete_dest(my_destinations)
         elif selection == '4':
-            print(my_destinations)
+            ic(my_destinations)
         elif selection == '5':
             search(my_destinations)
         elif selection == '6':
             high_score(my_destinations)
         elif selection == '7':
-            print('Program Terminated!!!')
+            ic('Program Terminated!!!')
             save(my_destinations)
             exit()
 
